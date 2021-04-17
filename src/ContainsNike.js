8251e6a69bcd38d8a9ae1e168e0ea7360593c0f8
bagotaj <bagotaj@gmail.com>
2021-04-17 18:05:04 +0200

    Implementing ContainsNike component

--- src/ContainsNike.js
+++ src/ContainsNike.js
@@ -1,5 +1,21 @@
 import React from 'react';
+import Table from './components/Table';
 
-export default function ContainsNike() {
-  return <div>rhgkgl</div>;
+export default function ContainsNike({ products }) {
+  function getContainsNike() {
+    let listOfContainsNike = [];
+
+    products.forEach((product) => {
+      if (
+        product.name.toLowerCase().includes('nike') ||
+        product.description.toLowerCase().includes('nike')
+      ) {
+        listOfContainsNike.push(product);
+      }
+    });
+
+    return listOfContainsNike;
+  }
+
+  return <Table products={getContainsNike()} />;
 }
